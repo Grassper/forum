02a42ac7d30b06f6763df5db25bdd3fd3f0048fd
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-10-18 21:37:46 +0530

    integration of storybook in react native

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+export {default} from './storybook';
 
-export default function App() {
-  return (
-    <View>
-  
-    </View>
-  );
-}
+// const App = () => {
+//   return (<View></View>);
+// }
+
+// export default Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App
 
 const styles = StyleSheet.create({});
