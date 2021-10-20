e088706aeb20de4a6a0fac51070e19efc2c7da34
Grassper <rishi.bharath27@gmail.com>
2021-10-20 22:58:13 +0530

    fix: husky, precommit, unwanted deps removed

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+// By default, all files inside `node_modules` are not transformed. But some 3rd party
+// modules are published as untranspiled, Jest will not understand the code in these modules.
+// To overcome this, exclude these modules in the ignore pattern.
+const untranspiledModulePatterns = [
+  "(jest-)?react-native",
+  "@react-native-community",
+  "expo(nent)?",
+  "@expo(nent)?/.*",
+  "react-navigation",
+  "@react-navigation/.*",
+  "@unimodules/.*",
+  "unimodules",
+  "sentry-expo",
+  "native-base",
+  "react-native-svg",
+];
+
+const config = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    `node_modules/(?!${untranspiledModulePatterns.join("|")})`,
+  ],
+  collectCoverage: true,
+  testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageReporters: ["json-summary", "text", "lcov"],
+};
+
+module.exports = config;
