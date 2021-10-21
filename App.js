d8075d62e929bdd66331d8d323e7874709359d11
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-10-21 23:23:18 +0530

    set button component

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export {default} from './storybook';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Config from "react-native-config";
+// import Button from "./components/Button";
+// export { default } from '../storybook';
+import StorybookUIRoot from "./storybook";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import CommunityTile from "./components/Tiles/CommunityTile";
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-italic": require("./assets/fonts/Montserrat-Italic.ttf"),
+    "Montserrat-regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+};
+const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-// const App = () => {
-//   return (<View></View>);
-// }
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
-// export default Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App
+  return (
+    // <StorybookUIRoot/>
+    <View style={{ flex: 1 }}>
+      <CommunityTile/>
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({});
