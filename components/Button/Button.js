475199d6540a3b7425d2db0a9bd0df5cb9a45399
grassper <grassper@grasspers-Mac-mini.local>
2021-10-22 22:24:00 +0530

    feat: added fonts

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import style from "../../storybook/stories/CenterView/style";
 
 const Button = ({ text, type, onPress, image }) => {
   return (
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-italic": require("./assets/fonts/Montserrat-Italic.ttf"),
+    "Montserrat-regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+};
 
 const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hi from react native!</Text>
