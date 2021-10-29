9b0634b1661a648e7ba2ea63529783d952c1a9b9
grassper <grassper@blockscribers.com>
2021-10-29 14:15:02 +0530

    feat: added signed request

--- amplify/backend/function/createPostAndTimeline/src/index.js
+++ amplify/backend/function/createPostAndTimeline/src/index.js
@@ -6,10 +6,109 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-const AWSAppSyncClient = require("aws-appsync").default;
-const gql = require("graphql-tag");
+const axios = require("axios");
+const AWS = require("aws-sdk");
 
-const createPost = gql`
+const urlParse = require("url").URL;
+const appsyncUrl = process.env.API_EFORUM_GRAPHQLAPIENDPOINTOUTPUT;
+const region = process.env.REGION;
+const endpoint = new urlParse(appsyncUrl).hostname.toString();
+const apiKey = process.env.API_EFORUM_GRAPHQLAPIKEYOUTPUT;
+
+exports.handler = async (event, _, callback) => {
+  let postInput;
+  let postStats = {
+    likes: 0,
+    loves: 0,
+    supports: 0,
+    disLikes: 0,
+  };
+
+  if (event.arguments.type === "TEXT") {
+    postInput = {
+      type: event.arguments.type,
+      content: event.arguments.content,
+      authorId: event.arguments.authorId,
+      communityId: event.arguments.communityId,
+      ...postStats,
+    };
+  } else if (
+    event.arguments.type === "AUDIO" ||
+    event.arguments.type === "VIDEO" ||
+    event.arguments.type === "IMAGE"
+  ) {
+    if (!event.arguments.mediaUrl) {
+      callback(
+        `post type of ${event.arguments.type} should contain mediaUrl`,
+        null
+      );
+    }
+    postInput = {
+      type: event.arguments.type,
+      content: event.arguments.content,
+      mediaUrl: event.arguments.mediaUrl,
+      authorId: event.arguments.authorId,
+      communityId: event.arguments.communityId,
+      ...postStats,
+    };
+  } else if (event.arguments.type === "POLL") {
+    if (!event.arguments.poll) {
+      callback(
+        `post type of ${event.arguments.type} should contain arrays of poll`,
+        null
+      );
+    }
+    postInput = {
+      type: event.arguments.type,
+      content: event.arguments.content,
+      poll: event.arguments.poll,
+      authorId: event.arguments.authorId,
+      communityId: event.arguments.communityId,
+      ...postStats,
+    };
+  } else {
+    callback(
+      "post type should be any of text, audio, video, image, poll",
+      null
+    );
+  }
+
+  try {
+    const req = new AWS.HttpRequest(appsyncUrl, region);
+    req.method = "POST";
+    req.headers.host = endpoint;
+    req.headers["Content-Type"] = "application/json";
+    req.body = JSON.stringify({
+      query: createPost,
+      variables: { input: postInput },
+    });
+
+    const signer = new AWS.Signers.V4(req, "appsync", true);
+    signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+
+    const graphqlData = await axios({
+      method: "POST",
+      url: appsyncUrl,
+      data: req.body,
+      headers: req.headers,
+    });
+
+    const body = {
+      message: "successfully created post!",
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  } catch (err) {
+    console.log("error creating post: ", err);
+  }
+};
+
+const createPost = `
   mutation CreatePost(
     $input: CreatePostInput!
     $condition: ModelPostConditionInput
@@ -72,7 +171,7 @@ const createPost = gql`
   }
 `;
 
-const createTimeline = gql`
+const createTimeline = `
   mutation CreateTimeline(
     $input: CreateTimelineInput!
     $condition: ModelTimelineConditionInput
@@ -117,17 +216,3 @@ const createTimeline = gql`
     }
   }
 `;
-
-exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
-};
