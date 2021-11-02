6890c3b4ddbd7f7cd4923a443f4deb6f01c21c12
grassper <grassper@blockscribers.com>
2021-11-02 16:53:28 +0530

    refactor: outsourced About

---
+++ src/components/screens/Profile/About.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+import { colors } from "@/root/src/constants";
+
+const windowWidth = Dimensions.get("window").width;
+
+interface Props_ {}
+
+export const About: React.FC<Props_> = () => {
+  return (
+    <View style={styles.wrapper}>
+      <ScrollView style={styles.container}>
+        <Text numberOfLines={4} style={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id
+          quam id ipsum aliquet consequat non quis nisl. Integer et ultrices
+          mauris, ac semper nunc.
+        </Text>
+        <View style={styles.statsContainer}>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.count}>8</Text>
+            <Text style={styles.countText}>Post Loves</Text>
+          </View>
+          <View style={styles.statsItem}>
+            <Text style={styles.count}>10</Text>
+            <Text style={styles.countText}>Post Supports</Text>
+          </View>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.count}>8</Text>
+            <Text style={styles.countText}>Post Likes</Text>
+          </View>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.count}>8</Text>
+            <Text style={styles.countText}>Post Dislikes</Text>
+          </View>
+          <View style={styles.statsItem}>
+            <Text style={styles.count}>10</Text>
+            <Text style={styles.countText}>Comment Ups</Text>
+          </View>
+          <View style={styles.statsItem}>
+            <Text style={styles.count}>18</Text>
+            <Text style={styles.countText}>Comment Downs</Text>
+          </View>
+          <View style={styles.statsItem}>
+            <Text style={styles.count}>10</Text>
+            <Text style={styles.countText}>Badges Earned</Text>
+          </View>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.count}>8</Text>
+            <Text style={styles.countText}>Popularity Level</Text>
+          </View>
+          <View style={styles.statsItem}>
+            <Text style={styles.count}>10</Text>
+            <Text style={styles.countText}>Active Days</Text>
+          </View>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.count}>8</Text>
+            <Text style={styles.countText}>Last Active</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingVertical: 15,
+    width: "90%",
+  },
+  count: {
+    color: colors.green,
+    fontFamily: "mb",
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 5,
+  },
+  countText: {
+    color: colors.black,
+    fontFamily: "mr",
+    fontSize: 14,
+    lineHeight: 21,
+  },
+  description: {
+    fontFamily: "mr",
+    fontSize: 14,
+    lineHeight: 21,
+    marginBottom: 15,
+  },
+  statsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  statsItem: {
+    marginBottom: 15,
+    minWidth: windowWidth / 3,
+  },
+  wrapper: {
+    alignItems: "center",
+    backgroundColor: colors.white,
+    flex: 1,
+    width: "100%",
+  },
+});
--- src/components/screens/Profile/Profile.tsx
+++ src/components/screens/Profile/Profile.tsx
@@ -1,9 +1,10 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import React from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 import { colors } from "@/root/src/constants";
 
+import { About } from "./About";
 import { ProfileCard } from "./ProfileCard";
 
 interface Props_ {}
@@ -22,62 +23,6 @@ const Posts = () => {
   return (
     <View style={styles.container}>
       <Text>HI</Text>
-    </View>
-  );
-};
-
-const About = () => {
-  return (
-    <View style={styles.wrapper}>
-      <ScrollView style={styles.aboutContainer}>
-        <Text numberOfLines={4} style={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id
-          quam id ipsum aliquet consequat non quis nisl. Integer et ultrices
-          mauris, ac semper nunc.
-        </Text>
-        <View style={styles.statsContainer}>
-          <View style={[styles.statsItem]}>
-            <Text style={styles.count}>8</Text>
-            <Text style={styles.countText}>Post Loves</Text>
-          </View>
-          <View style={styles.statsItem}>
-            <Text style={styles.count}>10</Text>
-            <Text style={styles.countText}>Post Supports</Text>
-          </View>
-          <View style={[styles.statsItem]}>
-            <Text style={styles.count}>8</Text>
-            <Text style={styles.countText}>Post Likes</Text>
-          </View>
-          <View style={[styles.statsItem]}>
-            <Text style={styles.count}>8</Text>
-            <Text style={styles.countText}>Post Dislikes</Text>
-          </View>
-          <View style={styles.statsItem}>
-            <Text style={styles.count}>10</Text>
-            <Text style={styles.countText}>Comment Ups</Text>
-          </View>
-          <View style={styles.statsItem}>
-            <Text style={styles.count}>18</Text>
-            <Text style={styles.countText}>Comment Downs</Text>
-          </View>
-          <View style={styles.statsItem}>
-            <Text style={styles.count}>10</Text>
-            <Text style={styles.countText}>Badges Earned</Text>
-          </View>
-          <View style={[styles.statsItem]}>
-            <Text style={styles.count}>8</Text>
-            <Text style={styles.countText}>Popularity Level</Text>
-          </View>
-          <View style={styles.statsItem}>
-            <Text style={styles.count}>10</Text>
-            <Text style={styles.countText}>Active Days</Text>
-          </View>
-          <View style={[styles.statsItem]}>
-            <Text style={styles.count}>8</Text>
-            <Text style={styles.countText}>Last Active</Text>
-          </View>
-        </View>
-      </ScrollView>
     </View>
   );
 };
@@ -116,45 +61,5 @@ export const Profile: React.FC<Props_> = () => {
 };
 
 const styles = StyleSheet.create({
-  aboutContainer: {
-    backgroundColor: colors.white,
-    flex: 1,
-    paddingVertical: 15,
-    width: "90%",
-  },
   container: { backgroundColor: colors.white, flex: 1 },
-  count: {
-    color: colors.green,
-    fontFamily: "mb",
-    fontSize: 16,
-    lineHeight: 24,
-    marginBottom: 5,
-  },
-  countText: {
-    color: colors.black,
-    fontFamily: "mr",
-    fontSize: 14,
-    lineHeight: 21,
-  },
-  description: {
-    fontFamily: "mr",
-    fontSize: 14,
-    lineHeight: 21,
-    marginBottom: 15,
-  },
-  statsContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  },
-  statsItem: {
-    marginBottom: 15,
-    minWidth: windowWidth / 3,
-  },
-  wrapper: {
-    alignItems: "center",
-    backgroundColor: colors.white,
-    flex: 1,
-    width: "100%",
-  },
 });
