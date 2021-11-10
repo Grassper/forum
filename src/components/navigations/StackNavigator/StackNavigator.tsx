5a3bbb792adf65147f8ee449863991f95b4da5b5
grassper <grassper@blockscribers.com>
2021-11-10 16:38:56 +0530

    feat: title changed

--- src/components/navigations/StackNavigator/StackNavigator.tsx
+++ src/components/navigations/StackNavigator/StackNavigator.tsx
@@ -132,7 +132,7 @@ export const StackNavigator = () => {
           name="AddAndEditComment"
           component={AddAndEditComment}
           options={() => ({
-            title: "Add Comment",
+            title: "Comment",
             headerStyle: {
               backgroundColor: colors.green,
             },
