24a02d3b8fea4e394514f4fcb1498a6941269b7d
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-11-20 23:42:08 +0530

    feat: refract comments

---
+++ src/components/screens/Comment/commentListHeader.tsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Text } from "native-base";
+import React from "react";
+
+import { CommentCard } from "@/root/src/components/shared/Cards/CommentCard";
+
+export const commentListHeader: React.FC = () => {
+  return (
+    <Box>
+      <CommentCard />
+      <Box alignItems="center" bg="white" mt="2" pt="4">
+        <Flex width="90%" flexDirection="row" alignItems="flex-end">
+          <Text fontWeight="500" color="eGreen.400">
+            Replies
+          </Text>
+          <Text fontWeight="500" color="eGreen.400" fontSize="xs" ml="1">
+            24
+          </Text>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
