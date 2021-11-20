128be9828490700c3f4869bb97a0f761dd3004b5
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-11-20 21:02:16 +0530

    feat: set audio element

--- src/components/shared/Cards/audio.tsx
+++ src/components/shared/Cards/audio.tsx
@@ -1,6 +1,10 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Audio } from "expo-av";
+import { Box, Flex, Text as Tex } from "native-base";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { colors } from "@/root/src/constants";
 
 interface props {
   audioUri: string;
@@ -59,13 +63,21 @@ export const AudioComponent: React.FC<props> = ({ audioUri }) => {
   return (
     <View style={styles.outerContainer}>
       <View style={styles.container}>
-        <Text>guru</Text>
+        <Box pt="3" pb="3">
+          <Flex direction="row" pl="3" pr="3">
+            <Ionicons name="ios-pause" size={24} color={colors.white} />
+            <Tex>00:04</Tex>
+            <Tex>hellow</Tex>
+            <Tex>00:18</Tex>
+            <Ionicons name="volume-high-sharp" size={24} color={colors.white} />
+          </Flex>
+        </Box>
       </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: { backgroundColor: "gray", width: "90%" },
+  container: { backgroundColor: "gray", borderRadius: 20, width: "90%" },
   outerContainer: {
     alignItems: "center",
     justifyContent: "center",
