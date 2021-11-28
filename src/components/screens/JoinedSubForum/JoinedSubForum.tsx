2811dbbe2b51bb085d8aefddaba0d810d0f020ca
grassper <rishi.bharath27@gmail.com>
2021-11-28 10:30:56 +0530

    feat: joined Subforum screen added

--- src/components/screens/JoinedSubForum/JoinedSubForum.tsx
+++ src/components/screens/JoinedSubForum/JoinedSubForum.tsx
@@ -1,16 +1,85 @@
+import { AntDesign } from "@expo/vector-icons";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import {
+  CompositeNavigationProp,
+  DrawerActions,
+} from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Box, Icon, ScrollView } from "native-base";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 
-interface Props_ {}
+import {
+  DrawerParamList_,
+  SubForumStackParamList_,
+} from "@/root/src/components/navigations/Navigation";
+import { SearchBar } from "@/root/src/components/shared/SearchBar";
+import { CommunityTile } from "@/root/src/components/shared/Tile";
 
-export const JoinedSubForum: React.FC<Props_> = () => {
+type NavigationProp_ = CompositeNavigationProp<
+  StackNavigationProp<SubForumStackParamList_, "SubForum">,
+  DrawerNavigationProp<DrawerParamList_>
+>;
+
+interface Props_ {
+  navigation: NavigationProp_;
+}
+
+export const JoinedSubForum: React.FC<Props_> = ({ navigation }) => {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Icon
+          as={<AntDesign name="menuunfold" />}
+          size={5}
+          ml="4"
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          color="white"
+        />
+      ),
+      headerRight: () => (
+        <Icon
+          as={<AntDesign name="pluscircleo" />}
+          size={"20px"}
+          mr="4"
+          onPress={() =>
+            navigation.push("EditAndCreateSubForum", {
+              title: "Create Subforum",
+              action: "Add",
+            })
+          }
+          color="white"
+        />
+      ),
+    });
+  }, [navigation]);
   return (
-    <View style={styles.container}>
-      <Text>Hi from react native</Text>
-    </View>
+    <Box bg="white" flex="1" alignItems="center">
+      <Box width="90%" pt="20px">
+        <SearchBar />
+      </Box>
+      <Box width="100%">
+        <ScrollView>
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+          <CommunityTile onPress={() => {}} hideDivider />
+        </ScrollView>
+      </Box>
+    </Box>
   );
 };
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-});
