b75cebf4c0517e2e437c55386e9c39d1be92bc7d
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-12-03 14:30:21 +0530

    feat: set search functionality in the post search screen

--- src/components/screens/Explore/PostSearch.tsx
+++ src/components/screens/Explore/PostSearch.tsx
@@ -1,23 +1,181 @@
+import { GraphQLResult } from "@aws-amplify/api-graphql";
+import { useFocusEffect } from "@react-navigation/native";
+import { API } from "aws-amplify";
 import { Box, Text } from "native-base";
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, ListRenderItem } from "react-native";
 
-import { PostCardRenderer } from "@/root/src/components/shared/CardRenderer";
+// import { PostCardRenderer } from "@/root/src/components/shared/CardRenderer";
+import {
+  PostCard,
+  Props_ as PostCardProps_,
+} from "@/root/src/components/shared/Cards/PostCard";
 import { dummyData } from "@/root/src/data/dummyData";
 
 import { TabNavigatorExploreContext } from "./context";
 
 export const PostSearch: React.FC = () => {
   const searchValue = React.useContext(TabNavigatorExploreContext);
+  const [posts, setPosts] = React.useState<Item[]>([]);
+  const [nextToken, setNextToken] = React.useState<string>("");
+  const populateContent = React.useCallback(() => {
+    let isActive = true;
 
+    const fetchCall = async () => {
+      if (searchValue) {
+        const searchPostInput: searchPostsFetchInput_ = {
+          limit: 10,
+          searchcontent: searchValue,
+        };
+
+        const responseData = await searchPostsFetch(searchPostInput);
+        if (responseData && isActive) {
+          setPosts(responseData.items);
+          setNextToken(responseData.nextToken);
+        }
+      }
+    };
+    fetchCall();
+
+    return () => {
+      isActive = false;
+    };
+  }, [searchValue]);
+
+  useFocusEffect(populateContent);
+  const handlePagination = async () => {
+    if (nextToken && searchValue) {
+      const listPostInput: searchPostsFetchInput_ = {
+        searchcontent: searchValue,
+        limit: 10,
+        nextToken,
+      };
+
+      const responseData = await searchPostsFetch(listPostInput);
+
+      if (responseData) {
+        setPosts((prevState) => [...prevState, ...responseData.items]);
+        setNextToken(responseData.nextToken);
+      }
+    }
+  };
+  const PostCardRenderer: ListRenderItem<Item> = ({ item }) => {
+    return (
+      <PostCard
+        id={item.id}
+        subForum={item.community.name}
+        subForumId={item.community.id}
+        type={
+          (item.type.charAt(0) +
+            item.type.slice(1).toLowerCase()) as PostCardProps_["type"]
+        }
+        username={item.author.username}
+        contentText={item.content}
+        avatarUrl={item.author.profileImageUrl}
+        timeStamp={item.postedDate}
+        mediaS3Key={item.mediaS3Key}
+      />
+    );
+  };
   return (
     <Box>
-      {/*  <FlatList
-        data={dummyData}
+      <FlatList
+        data={posts}
         renderItem={PostCardRenderer}
         keyExtractor={(item) => item.id}
-    />*/}
-      <Text>hi</Text>
+        onEndReached={() => handlePagination()}
+      />
     </Box>
   );
 };
+/**
+ * api calls
+ */
+interface searchPostsFetchInput_ {
+  searchcontent: string;
+  limit: number;
+  nextToken?: string;
+}
+const searchPostsFetch = async (input: searchPostsFetchInput_) => {
+  try {
+    const listSearchUserData = (await API.graphql({
+      query: listPostByContent,
+      variables: input,
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    })) as GraphQLResult<searchPosts_>;
+
+    if (listSearchUserData.data?.listPosts) {
+      const postSearchData = listSearchUserData.data.listPosts;
+      return postSearchData;
+    }
+  } catch (err) {
+    console.error("Error occured while fetching posts in explore screen", err);
+  }
+};
+
+/**
+ * graphql queries and their types
+ * types pattern {queryName}_
+ * * note dash(_) at the end of type name
+ * order 1.queryType 2.graphql query
+ */
+
+interface searchPosts_ {
+  listPosts?: ListPosts;
+}
+
+interface ListPosts {
+  items: Item[];
+  nextToken: string;
+}
+
+interface Item {
+  id: string;
+  type: string;
+  content: string;
+  mediaS3Key: null;
+  postedDate: Date;
+  author: Author;
+  community: Community;
+}
+
+interface Author {
+  username: string;
+  profileImageUrl: string;
+}
+
+interface Community {
+  name: string;
+  id: string;
+}
+
+const listPostByContent = /* GraphQL */ `
+  query listPostByContent(
+    $searchcontent: String
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(
+      limit: $limit
+      filter: { content: { contains: $searchcontent } }
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        type
+        content
+        mediaS3Key
+        postedDate
+        author {
+          username
+          profileImageUrl
+        }
+        community {
+          name
+          id
+        }
+      }
+      nextToken
+    }
+  }
+`;
