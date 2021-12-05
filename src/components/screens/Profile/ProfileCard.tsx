f480ce2b30d594d64cbe6f4b30a947526f0f096d
grassper <rishi.bharath27@gmail.com>
2021-12-05 14:39:17 +0530

    feat: follow relationship flow completed

--- src/components/screens/Profile/ProfileCard.tsx
+++ src/components/screens/Profile/ProfileCard.tsx
@@ -3,12 +3,11 @@ import { useFocusEffect } from "@react-navigation/native";
 import { API } from "aws-amplify";
 import { format } from "date-fns";
 import { Box, Button, HStack, Pressable, Text, VStack } from "native-base";
-import React from "react";
+import React, { useState } from "react";
 import { SvgUri } from "react-native-svg";
 
 import { Skeleton } from "@/root/src/components/shared/Skeleton";
 import { UserContext } from "@/root/src/context";
-import { useToggle } from "@/root/src/hooks";
 
 interface Props_ {
   routeUserId: string;
@@ -26,7 +25,9 @@ interface State_ {
 }
 
 export const ProfileCard: React.FC<Props_> = ({ routeUserId }) => {
-  const [value, toggleValue] = useToggle(true);
+  const [relationship, setRelationship] = useState<"FOLLOW" | "NOTFOLLOW">(
+    "NOTFOLLOW"
+  );
 
   const {
     user: { id },
@@ -61,6 +62,30 @@ export const ProfileCard: React.FC<Props_> = ({ routeUserId }) => {
   }, [routeUserId]);
 
   useFocusEffect(populateContent);
+
+  const RelationshipHandler = () => {
+    if (routeUserId && id && routeUserId !== id) {
+      if (relationship === "NOTFOLLOW") {
+        setRelationship("FOLLOW");
+
+        // create relationship call
+        FollowRelationshipFetch({
+          followeeId: routeUserId,
+          followerId: id,
+          action: "ADD",
+        });
+      } else {
+        setRelationship("NOTFOLLOW");
+
+        // delete relationship call
+        FollowRelationshipFetch({
+          followeeId: routeUserId,
+          followerId: id,
+          action: "DELETE",
+        });
+      }
+    }
+  };
 
   return (
     <Box alignItems="center" mt="5">
@@ -102,14 +127,14 @@ export const ProfileCard: React.FC<Props_> = ({ routeUserId }) => {
       {routeUserId && routeUserId !== id && profile?.userMetrics && (
         // checking our user id with incoming user id to show follow button
         <Button
-          onPress={() => toggleValue()}
-          bg={value ? "tertiary.500" : "danger.500"}
+          onPress={RelationshipHandler}
+          bg={relationship === "NOTFOLLOW" ? "tertiary.500" : "danger.500"}
           variant="unstyled"
           mb="5"
           minWidth="24"
           borderRadius="50"
         >
-          {value ? "Follow" : "Unfollow"}
+          {relationship === "NOTFOLLOW" ? "Follow" : "Unfollow"}
         </Button>
       )}
       <HStack alignItems="center" justifyContent="center" mb="15px">
@@ -187,10 +212,121 @@ const StatsCard: React.FC<StatsCard_> = ({ onPress, count, countName }) => {
 };
 
 /**
+ * api
+ */
+
+interface CreateFollowRelationshipFetchInput_ {
+  followerId: string;
+  followeeId: string;
+}
+
+interface DeleteFollowRelationshipFetchInput_ {
+  id: string;
+  followerId: string;
+  followeeId: string;
+  isDeleted: boolean;
+}
+
+interface FollowRelationshipFetchInput_ {
+  followerId: string;
+  followeeId: string;
+  action: "ADD" | "DELETE";
+}
+
+const FollowRelationshipFetch = async (
+  input: FollowRelationshipFetchInput_
+) => {
+  try {
+    // check if follow relationship exist
+
+    const isRelationshipExist = (await API.graphql({
+      query: CheckFollowRelationship,
+      variables: {
+        followeeId: input.followeeId,
+        followerId: input.followerId,
+      },
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    })) as GraphQLResult<CheckFollowRelationship_>;
+
+    const isRelationshipExistItems =
+      isRelationshipExist.data?.listFollowRelationships.items;
+
+    // if relationship doesnt exist, create on
+    if (
+      isRelationshipExistItems &&
+      isRelationshipExistItems.length === 0 &&
+      input.action === "ADD"
+    ) {
+      const createRelationshipInput: CreateFollowRelationshipFetchInput_ = {
+        followeeId: input.followeeId,
+        followerId: input.followerId,
+      };
+
+      const createdRelationship = (await API.graphql({
+        query: CreateFollowRelationship,
+        variables: {
+          input: createRelationshipInput,
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      })) as GraphQLResult<CreateFollowRelationship_>;
+
+      if (createdRelationship.data?.createFollowRelationship) {
+        return createdRelationship.data.createFollowRelationship.id;
+      }
+    } else if (
+      isRelationshipExistItems &&
+      isRelationshipExistItems.length === 1 &&
+      input.action === "DELETE"
+    ) {
+      const deletedRelationshipInput: DeleteFollowRelationshipFetchInput_ = {
+        id: isRelationshipExistItems[0].id,
+        followeeId: input.followeeId,
+        followerId: input.followerId,
+        isDeleted: true,
+      };
+
+      const deletedRelationship = (await API.graphql({
+        query: DeleteFollowRelationship,
+        variables: {
+          input: deletedRelationshipInput,
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      })) as GraphQLResult<DeleteFollowRelationship_>;
+
+      if (deletedRelationship.data?.updateFollowRelationship) {
+        return deletedRelationship.data.updateFollowRelationship.id;
+      }
+    }
+  } catch (err) {
+    console.error(
+      "Error occured while creating or deleting follow relationship in profile card",
+      err
+    );
+  }
+};
+
+/**
  * graphql queries and their types
  * types pattern {queryName}_
  * * note dash(_) at the end of type name
  * order 1.queryType 2.graphql query
+ */
+
+/**
+ * Todo-1: create, delete graphql schema and types
+ * Todo-2: followers and following metrics resolvers
+ */
+
+/**
+ * if user click follow button create the user follow relationships.
+ * increment follower metrics in followee users metrics
+ * increment following metrics in follower users metrics
+ */
+
+/**
+ * if user click unfollow button delete the user follow relationships.
+ * decrement follower metrics in followee users metrics
+ * decrement following metrics in follower users metrics
  */
 
 type getUser_ = {
@@ -207,6 +343,62 @@ const getUser = /* GraphQL */ `
         followers
         following
         totalPosts
+      }
+    }
+  }
+`;
+
+interface CreateFollowRelationship_ {
+  createFollowRelationship: Item;
+}
+
+interface DeleteFollowRelationship_ {
+  updateFollowRelationship: Item;
+}
+
+interface CheckFollowRelationship_ {
+  listFollowRelationships: { items: Item[] };
+}
+
+interface Item {
+  id: string;
+  followerId: string;
+  followeeId: string;
+}
+
+const CreateFollowRelationship = /* GraphQL */ `
+  mutation CreateFollowRelationship($input: CreateFollowRelationshipInput!) {
+    createFollowRelationship(input: $input) {
+      id
+      followerId
+      followeeId
+    }
+  }
+`;
+
+const DeleteFollowRelationship = /* GraphQL */ `
+  mutation UpdateFollowRelationship($input: UpdateFollowRelationshipInput!) {
+    updateFollowRelationship(input: $input) {
+      id
+      followerId
+      followeeId
+    }
+  }
+`;
+
+const CheckFollowRelationship = /* GraphQL */ `
+  query CheckFollowRelationship($followeeId: ID!, $followerId: ID!) {
+    listFollowRelationships(
+      filter: {
+        isDeleted: { attributeExists: false }
+        followeeId: { eq: $followeeId }
+        followerId: { eq: $followerId }
+      }
+    ) {
+      items {
+        id
+        followerId
+        followeeId
       }
     }
   }
