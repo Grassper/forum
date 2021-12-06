af7212e0805faaa7b0dfdc4adeaefbd8386ad970
staticGuru <79417096+staticGuru@users.noreply.github.com>
2021-12-07 02:24:18 +0530

    feat: set the following user list

--- src/components/screens/Follow/Follow.tsx
+++ src/components/screens/Follow/Follow.tsx
@@ -1,6 +1,12 @@
+import { GraphQLResult } from "@aws-amplify/api-graphql";
 import { DrawerNavigationProp } from "@react-navigation/drawer";
-import { CompositeNavigationProp, RouteProp } from "@react-navigation/native";
+import {
+  CompositeNavigationProp,
+  RouteProp,
+  useFocusEffect,
+} from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { API } from "aws-amplify";
 import { Box } from "native-base";
 import React from "react";
 
@@ -21,9 +27,36 @@ interface Props_ {
   route: RouteProp_;
 }
 
-interface Props_ {}
+export const Follow: React.FC<Props_> = ({ navigation, route }) => {
+  const title = route.params?.title;
+  const routeUserId = route.params?.routeUserId;
+  const [usersList, setUsersList] = React.useState<Item[]>([]);
+  const [nextToken, setNextToken] = React.useState<string>("");
 
-export const Follow: React.FC<Props_> = () => {
+  const populateContent = React.useCallback(() => {
+    let isActive = true;
+
+    const fetchCall = async () => {
+      if (title === "Following") {
+        const follwersInput: followingFetchInput_ = {
+          limit: 10,
+          id: routeUserId,
+          nextToken,
+        };
+
+        const responseData = await followingFetch(follwersInput);
+        if (responseData && isActive) {
+          setUsersList(responseData.items);
+          // setNextToken(responseData.items.);
+        }
+      }
+    };
+    fetchCall();
+
+    return () => {
+      isActive = false;
+    };
+  }, [title]);
   return <Box bg="white" flex="1" alignItems="center" />;
 };
 
@@ -51,6 +84,57 @@ const GetFollowersByUserId = /* GraphQL */ `
     }
   }
 `;
+
+/**
+ * api calls
+ */
+interface followingFetchInput_ {
+  limit: number;
+  id: string;
+  nextToken?: string;
+}
+
+const followingFetch = async (input: followingFetchInput_) => {
+  try {
+    const listFollowingUserData = (await API.graphql({
+      query: GetFollowingByUserId,
+      variables: input,
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    })) as GraphQLResult<GetFollowingByUserId_>;
+
+    if (listFollowingUserData.data?.getUser) {
+      const followingData = listFollowingUserData.data.getUser.followees;
+      return followingData;
+    }
+  } catch (err) {
+    console.error(
+      "Error occured while fetch the following in follow screen",
+      err
+    );
+  }
+};
+
+interface GetFollowingByUserId_ {
+  getUser?: GetUser;
+}
+
+interface GetUser {
+  followees: Followees;
+}
+
+interface Followees {
+  items: Item[];
+}
+
+interface Item {
+  followee: Followee;
+}
+
+interface Followee {
+  id: string;
+  username: string;
+  profileImageUrl: string;
+}
 
 const GetFollowingByUserId = /* GraphQL */ `
   query getFollowing($id: ID!, $nextToken: String, $limit: Int) {
