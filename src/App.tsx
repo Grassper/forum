17c8581b22ac53133767cdb1cb54f78cbccb50a6
grassper <rishi.bharath27@gmail.com>
2021-12-09 11:12:50 +0530

    chore: revert changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { enableScreens } from "react-native-screens";
 
 import { RootStackNavigator } from "@/root/src/components/navigations/RootStackNavigator";
@@ -22,151 +21,137 @@ Amplify.configure({ ...config, Analytics: { disabled: true } });
 
 enableScreens();
 
-// const fetchFonts = (): Promise<void> => {
-//   return Font.loadAsync({
-//     ml: require("@/root/assets/fonts/Montserrat-Light.ttf"),
-//     mr: require("@/root/assets/fonts/Montserrat-Regular.ttf"),
-//     mm: require("@/root/assets/fonts/Montserrat-Medium.ttf"),
-//     ms: require("@/root/assets/fonts/Montserrat-SemiBold.ttf"),
-//     mb: require("@/root/assets/fonts/Montserrat-Bold.ttf"),
-//     rl: require("@/root/assets/fonts/RobotoSlab-Light.ttf"),
-//     rr: require("@/root/assets/fonts/RobotoSlab-Regular.ttf"),
-//     rm: require("@/root/assets/fonts/RobotoSlab-Medium.ttf"),
-//     rs: require("@/root/assets/fonts/RobotoSlab-SemiBold.ttf"),
-//     rb: require("@/root/assets/fonts/RobotoSlab-Bold.ttf"),
-//   });
-// };
-
-// const App: React.FC = () => {
-//   const [fontLoaded, setFontLoaded] = React.useState(false);
-//   const { user, updateUser } = React.useContext(UserContext);
-
-//   React.useEffect(() => {
-//     const checkCurrentUserInDb = async () => {
-//       try {
-//         // get authenticated user from cognito
-//         const currentUser = await Auth.currentAuthenticatedUser({
-//           bypassCache: true,
-//         });
-
-//         if (currentUser) {
-//           // check user exist in db
-//           const userData = (await API.graphql({
-//             query: getUser,
-//             variables: { id: currentUser.attributes.sub },
-//             authMode: "AMAZON_COGNITO_USER_POOLS",
-//           })) as GraphQLResult<getUser_>;
-
-//           if (userData.data?.getUser) {
-//             // if user already exist set state and return
-//             const { profileImageUrl, coins, about, _version } =
-//               userData.data.getUser;
-
-//             updateUser({
-//               id: currentUser.attributes.sub,
-//               username: currentUser.username,
-//               email: currentUser.attributes.email,
-//               about,
-//               profileImageUrl,
-//               coins,
-//               _version,
-//             });
-
-//             return;
-//           }
-
-//           // if user is not registered. register user to db
-//           const newUser = {
-//             id: currentUser.attributes.sub,
-//             username: currentUser.username,
-//             email: currentUser.attributes.email,
-//             profileImageUrl: `https://avatars.dicebear.com/api/micah/${currentUser.attributes.sub}.svg`, // for user profile replace space with dash
-//             coins: 1000,
-//             about: "Hooray, Finally i'm am here",
-//           };
-
-//           const newUserMetrics = {
-//             id: currentUser.attributes.sub,
-//             postLikes: 0,
-//             postLoves: 0,
-//             postSupports: 0,
-//             postDislikes: 0,
-//             profileViews: 0,
-//             commentUpvotes: 0,
-//             commentDownvotes: 0,
-//             followers: 0,
-//             following: 0,
-//             totalPosts: 0,
-//             totalComments: 0,
-//             communitiesJoined: 0,
-//             communitiesModerating: 0,
-//             activeDays: 0,
-//             lastActiveDay: new Date().toISOString(),
-//           };
-
-//           await API.graphql({
-//             query: createUser,
-//             variables: { input: newUser },
-//             authMode: "AMAZON_COGNITO_USER_POOLS",
-//           });
-
-//           await API.graphql({
-//             query: createUserMetrics,
-//             variables: { input: newUserMetrics },
-//             authMode: "AMAZON_COGNITO_USER_POOLS",
-//           });
-//         }
-//       } catch (err) {
-//         console.error("error while registering user in app.tsx", err);
-//       }
-//     };
-//     checkCurrentUserInDb();
-//   }, [updateUser]);
-
-//   if (!fontLoaded) {
-//     return (
-//       <AppLoading
-//         startAsync={fetchFonts}
-//         onFinish={() => setFontLoaded(true)}
-//         onError={console.warn}
-//       />
-//     );
-//   }
-
-//   if (!user.id) {
-//     return <AppLoading />;
-//   }
-
-//   return (
-//     <NativeBaseProvider theme={Theme}>
-//       <RootStackNavigator />
-//     </NativeBaseProvider>
-//   );
-// };
+const fetchFonts = (): Promise<void> => {
+  return Font.loadAsync({
+    ml: require("@/root/assets/fonts/Montserrat-Light.ttf"),
+    mr: require("@/root/assets/fonts/Montserrat-Regular.ttf"),
+    mm: require("@/root/assets/fonts/Montserrat-Medium.ttf"),
+    ms: require("@/root/assets/fonts/Montserrat-SemiBold.ttf"),
+    mb: require("@/root/assets/fonts/Montserrat-Bold.ttf"),
+    rl: require("@/root/assets/fonts/RobotoSlab-Light.ttf"),
+    rr: require("@/root/assets/fonts/RobotoSlab-Regular.ttf"),
+    rm: require("@/root/assets/fonts/RobotoSlab-Medium.ttf"),
+    rs: require("@/root/assets/fonts/RobotoSlab-SemiBold.ttf"),
+    rb: require("@/root/assets/fonts/RobotoSlab-Bold.ttf"),
+  });
+};
 
 const App: React.FC = () => {
+  const [fontLoaded, setFontLoaded] = React.useState(false);
+  const { user, updateUser } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    const checkCurrentUserInDb = async () => {
+      try {
+        // get authenticated user from cognito
+        const currentUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+
+        if (currentUser) {
+          // check user exist in db
+          const userData = (await API.graphql({
+            query: getUser,
+            variables: { id: currentUser.attributes.sub },
+            authMode: "AMAZON_COGNITO_USER_POOLS",
+          })) as GraphQLResult<getUser_>;
+
+          if (userData.data?.getUser) {
+            // if user already exist set state and return
+            const { profileImageUrl, coins, about, _version } =
+              userData.data.getUser;
+
+            updateUser({
+              id: currentUser.attributes.sub,
+              username: currentUser.username,
+              email: currentUser.attributes.email,
+              about,
+              profileImageUrl,
+              coins,
+              _version,
+            });
+
+            return;
+          }
+
+          // if user is not registered. register user to db
+          const newUser = {
+            id: currentUser.attributes.sub,
+            username: currentUser.username,
+            email: currentUser.attributes.email,
+            profileImageUrl: `https://avatars.dicebear.com/api/micah/${currentUser.attributes.sub}.svg`, // for user profile replace space with dash
+            coins: 1000,
+            about: "Hooray, Finally i'm am here",
+          };
+
+          const newUserMetrics = {
+            id: currentUser.attributes.sub,
+            postLikes: 0,
+            postLoves: 0,
+            postSupports: 0,
+            postDislikes: 0,
+            profileViews: 0,
+            commentUpvotes: 0,
+            commentDownvotes: 0,
+            followers: 0,
+            following: 0,
+            totalPosts: 0,
+            totalComments: 0,
+            communitiesJoined: 0,
+            communitiesModerating: 0,
+            activeDays: 0,
+            lastActiveDay: new Date().toISOString(),
+          };
+
+          await API.graphql({
+            query: createUser,
+            variables: { input: newUser },
+            authMode: "AMAZON_COGNITO_USER_POOLS",
+          });
+
+          await API.graphql({
+            query: createUserMetrics,
+            variables: { input: newUserMetrics },
+            authMode: "AMAZON_COGNITO_USER_POOLS",
+          });
+        }
+      } catch (err) {
+        console.error("error while registering user in app.tsx", err);
+      }
+    };
+    checkCurrentUserInDb();
+  }, [updateUser]);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  if (!user.id) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hi from react native</Text>
-    </View>
+    <NativeBaseProvider theme={Theme}>
+      <RootStackNavigator />
+    </NativeBaseProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  container: { alignItems: "center", flex: 1, justifyContent: "center" },
-});
+const ContextWrapper: React.FC = () => {
+  return (
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  );
+};
 
-export default App;
-
-// const ContextWrapper: React.FC = () => {
-//   return (
-//     <UserContextProvider>
-//       <App />
-//     </UserContextProvider>
-//   );
-// };
-
-// export default withAuthenticator(ContextWrapper);
+export default withAuthenticator(ContextWrapper);
 
 /**
  * graphql queries and their types
