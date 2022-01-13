0953af28ff8054839a7919ef168087bd810dd542
staticGuru <79417096+staticGuru@users.noreply.github.com>
2022-01-13 22:52:41 +0530

    feat: implented explore all screen

---
+++ src/components/screens/Explore/ExploreAll.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "native-base";
+import React from "react";
+
+interface Props_ {}
+export const ExploreAll: React.FC<Props_> = () => {
+  return (
+    <Box>
+      <Text>explore screen</Text>
+    </Box>
+  );
+};
--- src/components/screens/Explore/index.ts
+++ src/components/screens/Explore/index.ts
@@ -1,3 +1,4 @@
 export * from "./Explore";
+export * from "./ExploreAll";
 export * from "./PostSearch";
 export * from "./ProfileSearch";
